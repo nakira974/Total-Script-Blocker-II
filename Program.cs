6e12a7424a02b198e27279ec880909941da36e07
nakira974 <maxime.loukhal@gmail.com>
2022-09-02 22:25:05 +0200

    functionnal wasm need to fix Background.razor with background.js

--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Total_Script_Blocker_II;
+using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace Total_Script_Blocker_II
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddOidcAuthentication(options =>
-                                       {
-                                           // Configure your authentication provider options here.
-                                           // For more information, see https://aka.ms/blazor-standalone-auth
-                                           builder.Configuration.Bind("Local", options.ProviderOptions);
-                                       });
-
-builder.Services.AddBrowserExtensionServices();
-await builder.Build().RunAsync();
+            builder.Services.AddBrowserExtensionServices();
+            await builder.Build().RunAsync();
+        }
+    }
+}
